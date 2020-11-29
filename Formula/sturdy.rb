af3ba205301ffbfb6bd716f2508301392628c740
Gustav Westling <gustav@westling.dev>
2020-11-29 19:35:52 +0100

    Sturdy v0.1.5

--- Formula/sturdy.rb
+++ Formula/sturdy.rb
@@ -1,15 +1,15 @@
 class Sturdy < Formula
     desc "Sturdy Client"
     homepage "https://driva.dev/"
-    version "0.1.4"
+    version "0.1.5"
     bottle :unneeded
 
     if OS.mac?
-        url "https://driva.dev/client/sturdy-v0.1.4-darwin-amd64.tar.gz"
-        sha256 "3c5f7cae309217b5ca68b02a0218d3d9786086e86f6206277bcc447941cb928e"
+        url "https://driva.dev/client/sturdy-v0.1.5-darwin-amd64.tar.gz"
+        sha256 "9fa532f878663ae606315cc9abdb1dcec95f8e29b57625ca0018fe95b84946e3"
     elsif OS.linux?
-        url "https://driva.dev/client/sturdy-v0.1.4-linux-amd64.tar.gz"
-        sha256 "4cfd1e02ed50c5f016a2b7685b0d1fd540c9de57ab5139fc3b534fc1cce75034"
+        url "https://driva.dev/client/sturdy-v0.1.5-linux-amd64.tar.gz"
+        sha256 "d55447e083b1026f3b2e5db04bb94232cd3175b8d48fd96dcefa9b40fadcf3c5"
     end
 
     def install
