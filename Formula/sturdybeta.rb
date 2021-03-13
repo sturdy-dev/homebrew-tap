19ba849809d6aaac89fe29ea55abd4cfaad27a25
Gustav Westling <gustav@westling.dev>
2021-03-13 23:05:40 +0100

    sturdybeta v0.4.6

--- Formula/sturdybeta.rb
+++ Formula/sturdybeta.rb
@@ -6,10 +6,10 @@ class Sturdybeta < Formula
 
     if OS.mac?
         url "https://getsturdy.com/client/sturdy-v0.4.6-darwin-amd64.tar.gz"
-        sha256 "709fb6cce1281a33c92ac51577eba708d382f2f4db2b919f8bc5dc2883781ac2"
+        sha256 "88ed8ee7df427fc2a89826a3c5bd6b09a2e5153845d2afb4d693b513c310e87e"
     elsif OS.linux?
         url "https://getsturdy.com/client/sturdy-v0.4.6-linux-amd64.tar.gz"
-        sha256 "e467be342b80d22ee781f0369cdc2a2c2a1b4c4ab34e966d1175dbcbd2b3caae"
+        sha256 "dfac73199e8cdf47ed7832ad067b582beeb5d228fb974d819c399da9960b3090"
     end
 
     def install
