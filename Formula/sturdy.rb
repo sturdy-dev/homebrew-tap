4d320fd157dd95b6fd87db811c6084d14c4c9203
Gustav Westling <gustav@westling.dev>
2021-05-04 09:30:47 +0200

    sturdy v0.5.7

--- Formula/sturdy.rb
+++ Formula/sturdy.rb
@@ -1,15 +1,15 @@
 class Sturdy < Formula
     desc "Sturdy Client"
     homepage "https://getsturdy.com/"
-    version "0.5.6"
+    version "0.5.7"
     bottle :unneeded
 
     if OS.mac?
-        url "https://getsturdy.com/client/sturdy-v0.5.6-darwin-amd64.tar.gz"
-        sha256 "6eb332e378fd6de957b25d9dc7b1a0900f47866a917124ce243102b9dc4bf991"
+        url "https://getsturdy.com/client/sturdy-v0.5.7-darwin-amd64.tar.gz"
+        sha256 "bd74c5a215b415552a61f422bef8fce204c0141f7a24700177706998e2486c83"
     elsif OS.linux?
-        url "https://getsturdy.com/client/sturdy-v0.5.6-linux-amd64.tar.gz"
-        sha256 "ef33be4e574e8952ffbda1a0a896fa343c08775d33ab81501900b8a6610d8c0f"
+        url "https://getsturdy.com/client/sturdy-v0.5.7-linux-amd64.tar.gz"
+        sha256 "44260abcbd5c47825b3ae359b66592aaf13d89111b5d6bbbff96cc2a6717d662"
     end
 
     def install
