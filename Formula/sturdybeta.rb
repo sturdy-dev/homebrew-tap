13191cc9ebe1fec33e7be123d886b8303384c475
Gustav Westling <gustav@westling.dev>
2021-09-29 11:02:38 +0200

    sturdy v0.7.1-beta0

--- Formula/sturdybeta.rb
+++ Formula/sturdybeta.rb
@@ -1,15 +1,18 @@
 class Sturdybeta < Formula
     desc "Sturdy Client"
     homepage "https://getsturdy.com/"
-    version "0.7.0-beta0"
+    version "0.7.1-beta0"
     bottle :unneeded
 
-    if OS.mac?
-        url "https://getsturdy.com/client/sturdy-v0.7.0-beta0-darwin-amd64.tar.gz"
-        sha256 "e0f9e4259d7e281a1297cacc78500ac5c1558c403701471c7c7aef00ef29b0e3"
+    if OS.mac? && Hardware::CPU.intel?
+        url "https://getsturdy.com/client/sturdy-v0.7.1-beta0-darwin-amd64.tar.gz"
+        sha256 "da00b395efe0fb510cf713531efd4c1be171d23cb8af9a1af4fcaccfba132253"
+    if OS.mac? && Hardware::CPU.arm64?
+        url "https://getsturdy.com/client/sturdy-v0.7.1-beta0-darwin-arm64.tar.gz"
+        sha256 "692f0b33d3dec291d5b249582ab33a9df40510866b6c9f8e459fa5f9c6e5ad47"
     elsif OS.linux?
-        url "https://getsturdy.com/client/sturdy-v0.7.0-beta0-linux-amd64.tar.gz"
-        sha256 "6c5f562806db7e1ae7081dc66fb3d034d2d3a9a228fede488e3339d9d0e06aee"
+        url "https://getsturdy.com/client/sturdy-v0.7.1-beta0-linux-amd64.tar.gz"
+        sha256 "b0cf33f11076c506a07e9e2c0096339aa4bb05f5658674bd2e119cb2e01fde8a"
     end
 
     def install
