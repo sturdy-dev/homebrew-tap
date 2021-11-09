f76099fbf46720f1f12554420ac6cb85c930f5d4
Nikita Galaiko <nikita@galaiko.rocks>
2021-11-09 17:42:17 +0100

    sturdy v0.7.6-beta1

--- Formula/sturdybeta.rb
+++ Formula/sturdybeta.rb
@@ -1,19 +1,19 @@
 class Sturdybeta < Formula
     desc "Sturdy Client"
     homepage "https://getsturdy.com/"
-    version "0.7.6-beta0"
+    version "0.7.6-beta1"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://getsturdy.com/client/sturdy-v0.7.6-beta0-darwin-amd64.tar.gz"
+        url "https://getsturdy.com/client/sturdy-v0.7.6-beta1-darwin-amd64.tar.gz"
         sha256 ""
     elsif OS.mac? && Hardware::CPU.arm?
-        url "https://getsturdy.com/client/sturdy-v0.7.6-beta0-darwin-arm64.tar.gz"
+        url "https://getsturdy.com/client/sturdy-v0.7.6-beta1-darwin-arm64.tar.gz"
         sha256 ""
     elsif OS.linux? && Hardware::CPU.intel?
-        url "https://getsturdy.com/client/sturdy-v0.7.6-beta0-linux-amd64.tar.gz"
+        url "https://getsturdy.com/client/sturdy-v0.7.6-beta1-linux-amd64.tar.gz"
         sha256 ""
     elsif OS.linux? && Hardware::CPU.arm?
-        url "https://getsturdy.com/client/sturdy-v0.7.6-beta0-linux-arm64.tar.gz"
+        url "https://getsturdy.com/client/sturdy-v0.7.6-beta1-linux-arm64.tar.gz"
         sha256 ""
     end
 
